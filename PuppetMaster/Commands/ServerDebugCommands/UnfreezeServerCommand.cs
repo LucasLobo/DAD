ee7839281229d2c8d0ef523e55908e341ef4be7c
BFreitas16 <bruno_freitas_8@hotmail.com>
2020-10-11 17:04:49 +0100

    unfreeze script command. Closes #44

---
+++ PuppetMaster/Commands/ServerDebugCommands/UnfreezeServerCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PuppetMaster.Commands
+{
+    class UnfreezeServerCommand : Command
+    {
+        private TextBox txtBoxOutput;
+        public UnfreezeServerCommand(TextBox output)
+        {
+            this.txtBoxOutput = output;
+        }
+
+        public static int EXPECTED_ARGUMENTS = 1;
+        public override void Execute(List<string> arguments)
+        {
+            if (arguments.Count != EXPECTED_ARGUMENTS)
+            {
+                this.txtBoxOutput.AppendText(Environment.NewLine + "Expected " + EXPECTED_ARGUMENTS + " arguments but found " + arguments.Count + ".");
+                return;
+            }
+
+            // Dummy implementation
+            this.txtBoxOutput.AppendText(Environment.NewLine + "Unfreeze DONE.");
+        }
+    }
+}

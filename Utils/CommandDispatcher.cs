62a158cc3b24cad69db28c5124d8af190687455b
Lucas Lobo Fell <lucas5798fell@gmail.com>
2020-10-11 20:47:54 +0100

    Create utils project. Closes #64

---
+++ Utils/CommandDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public class CommandDispatcher
+    {
+
+        private readonly Dictionary<string, Command> commandMap = new Dictionary<string, Command>();
+
+        public void Register(string commandName, Command command)
+        {
+            commandMap.Add(commandName, command);
+        }
+
+        public void Execute(string commandName, List<string> arguments)
+        {
+            commandMap.TryGetValue(commandName, out Command command);
+            if (command == null)
+            {
+                Console.WriteLine("Unknown command. Use \"?\" for help."); //todo ?
+                return;
+            }
+            command.Execute(arguments);
+        }
+    }
+}

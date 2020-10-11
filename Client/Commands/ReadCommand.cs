2f6d87d71a56fcc3b119c0057d0b88359751bc77
Lucas Lobo Fell <lucas5798fell@gmail.com>
2020-10-11 20:55:34 +0100

    Create Read Command. Closes #7

---
+++ Client/Commands/ReadCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Utils;
+
+namespace Client.Commands
+{
+    class ReadCommand : Command
+    {
+        public static int EXPECTED_ARGUMENTS = 3;
+
+        public override void Execute(List<string> arguments)
+        {
+            if (arguments.Count != EXPECTED_ARGUMENTS)
+            {
+                Console.WriteLine("Expected " + EXPECTED_ARGUMENTS + " arguments but found " + arguments.Count + ".");
+                return;
+            }
+            Console.WriteLine("Processing...");
+        }
+    }
+}

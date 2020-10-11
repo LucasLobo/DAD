593775102d27f89caa1fb4ba5e0aafad213a804f
BFreitas16 <bruno_freitas_8@hotmail.com>
2020-10-11 17:00:45 +0100

    wait script command implemented. Closes #35

---
+++ PuppetMaster/Commands/HelpCommands/WaitCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+using System.Linq;
+
+namespace PuppetMaster.Commands
+{
+    class WaitCommand : Command
+    {
+        private TextBox txtBoxOutput;
+        public WaitCommand(TextBox output)
+        {
+            this.txtBoxOutput = output;
+        }
+
+        public static int EXPECTED_ARGUMENTS = 1;
+        public override void Execute(List<string> arguments)
+        {
+            if (arguments.Count != EXPECTED_ARGUMENTS)
+            {
+                this.txtBoxOutput.AppendText(Environment.NewLine + "Expected " + EXPECTED_ARGUMENTS + " arguments but found " + arguments.Count + ".");
+                return;
+            }
+
+            try
+            {
+                int milliseconds = Int32.Parse(arguments.ElementAt(0));
+                this.txtBoxOutput.AppendText(Environment.NewLine + "Sleeping...");
+                Thread.Sleep(milliseconds);
+            }
+            catch (FormatException)
+            {
+                this.txtBoxOutput.AppendText(Environment.NewLine + "Argument must be of type int.");
+                return;
+            }
+
+        }
+    }
+}

8088a4723742965b85a68a99df96010b81158daa
BFreitas16 <bruno_freitas_8@hotmail.com>
2020-10-11 17:05:51 +0100

    status script command. Closes #45

---
+++ PuppetMaster/Commands/PCSCommands/StatusCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PuppetMaster.Commands
+{
+    class StatusCommand : Command
+    {
+        private TextBox txtBoxOutput;
+        public StatusCommand(TextBox output)
+        {
+            this.txtBoxOutput = output;
+        }
+
+        public static int EXPECTED_ARGUMENTS = 0;
+        public override void Execute(List<string> arguments)
+        {
+            if (arguments.Count != EXPECTED_ARGUMENTS)
+            {
+                this.txtBoxOutput.AppendText(Environment.NewLine + "Expected " + EXPECTED_ARGUMENTS + " arguments but found " + arguments.Count + ".");
+                return;
+            }
+
+            // Dummy implementation
+            this.txtBoxOutput.AppendText(Environment.NewLine + "Status DONE.");
+        }
+    }
+}
